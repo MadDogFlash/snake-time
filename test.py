015f50f0de27e100c26a96512b65664cf21f53b7
MadDogFlash <49573648+MadDogFlash@users.noreply.github.com>
2022-10-22 15:10:37 -0700

    update formating

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-def creating_list():
-    return [russia for in range (6)]
-    counts = creating_list()
-    print('The returnes list is: ', [1, 2 , 3 , 4, 5 , 6 ])
+def the_list():
+       list = []
+   for i in range(0,11):
+      # adding a new value to the end of the list
+      list.append(i)
+   return list
+a = the_list()
+print ('The returnes list is:',a)
